79b959d174c2c39751c3180f96666f82fa37055a
Eliot Hedeman <eliot.d.hedeman@gmail.com>
2025-06-12 22:37:05 -0400

    Make linter happy

--- check.go
+++ check.go
@@ -11,37 +11,37 @@ func formatCmp[T any](cmp string, a, b T) string {
 }
 
 func Eq[T comparable](a, b T) {
-	if !(a == b) {
+	if a != b {
 		panic(formatCmp("==", a, b))
 	}
 }
 
 func NotEq[T comparable](a, b T) {
-	if !(a != b) {
+	if a == b {
 		panic(formatCmp("!=", a, b))
 	}
 }
 
 func GT[T cmp.Ordered](a, b T) {
-	if !(a > b) {
+	if a <= b {
 		panic(formatCmp(">", a, b))
 	}
 }
 
 func LT[T cmp.Ordered](a, b T) {
-	if !(a < b) {
+	if a >= b {
 		panic(formatCmp("<", a, b))
 	}
 }
 
 func GTE[T cmp.Ordered](a, b T) {
-	if !(a >= b) {
+	if a < b {
 		panic(formatCmp(">=", a, b))
 	}
 }
 
 func LTE[T cmp.Ordered](a, b T) {
-	if !(a <= b) {
+	if a > b {
 		panic(formatCmp(">=", a, b))
 	}
 }
