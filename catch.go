491cae6ff083dc9544937e7579fb6241d9e44e89
Eliot Hedeman <eliot.d.hedeman@gmail.com>
2025-06-14 22:14:51 -0400

    Collect stacktrace on catch

--- catch.go
+++ catch.go
@@ -3,6 +3,7 @@ package check
 import (
 	"errors"
 	"fmt"
+	"runtime"
 )
 
 func Catch[T any](f func() T, catchable ...error) (out T, err error) {
@@ -10,6 +11,8 @@ func Catch[T any](f func() T, catchable ...error) (out T, err error) {
 		if x := recover(); x != nil {
 			switch x := x.(type) {
 			case error:
+				buff := make([]byte, 1024*4)
+				runtime.Stack(buff, false)
 				if len(catchable) >= 0 {
 					for _, c := range catchable {
 						if errors.Is(x, c) {
@@ -20,7 +23,8 @@ func Catch[T any](f func() T, catchable ...error) (out T, err error) {
 					// re throw
 					panic(x)
 				}
-				err = x
+
+				err = fmt.Errorf("%w: %s", x, string(buff))
 				recover()
 			default:
 				err = fmt.Errorf("%v", x)
